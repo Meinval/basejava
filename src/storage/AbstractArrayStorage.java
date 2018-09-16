07f5ed04591724f886776841b5492612e2b07167
Meinval <admin@logd.ru>
2018-09-16 17:53:05 +0300

    HW3 - add some javadoc and realuse update, save and delete method in AbstractArrayStorage

--- src/storage/AbstractArrayStorage.java
+++ src/storage/AbstractArrayStorage.java
@@ -21,11 +21,39 @@ public abstract class AbstractArrayStorage implements Storage {
 
     int size = 0;
 
+    /**
+     * Clear array. Also reset realSize
+     */
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
+    /**
+     * Update resume if it founded, else error message
+     *
+     * @param r inputed resume to update
+     */
+    public abstract void update(Resume r);
+
+    /**
+     * Add resume to array, increase array size counter
+     *
+     * @param r inputed resume to save
+     */
+    public abstract void save(Resume r);
+
+    /**
+     * Remove resume from array, descrease array size counter
+     *
+     * @param uuid inputed id name
+     */
+    public abstract void delete(String uuid);
+
+    /**
+     * @param uuid inputed id name
+     * @return element of array where id = uuid. If cant find, return null
+     */
     public Resume get(String uuid) {
         int index = getIndex(uuid);
         if (index == -1) {
@@ -35,12 +63,22 @@ public abstract class AbstractArrayStorage implements Storage {
         return storage[index];
     }
 
+    /**
+     * @param uuid inputed id name
+     * @return index of array where id = uuid.
+     */
     protected abstract int getIndex(String uuid);
 
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
     public Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, size);
     }
 
+    /**
+     * @return array size counter
+     */
     public int size() {
         return size;
     }
