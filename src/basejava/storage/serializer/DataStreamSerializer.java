f5b270fd29107f27a57b97fc6dd7174f2380082f
Meinval <admin@logd.ru>
2019-02-02 19:08:51 +0300

    HW10 - finish DataStreamSerializer

--- src/basejava/storage/serializer/DataStreamSerializer.java
+++ src/basejava/storage/serializer/DataStreamSerializer.java
@@ -1,9 +1,11 @@
 package basejava.storage.serializer;
 
-import basejava.model.ContactType;
-import basejava.model.Resume;
+import basejava.model.*;
 
 import java.io.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class DataStreamSerializer implements StreamSerializer {
@@ -19,7 +21,36 @@ public class DataStreamSerializer implements StreamSerializer {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
             }
-            // TODO implements sections
+            Map<SectionType, AbstractSection> sectionMap = r.getSectionsMap();
+            dos.writeInt(sectionMap.size());
+            for (Map.Entry<SectionType, AbstractSection> entry : sectionMap.entrySet()) {
+                dos.writeUTF(entry.getKey().name());
+                if (entry.getValue() instanceof TextSection) {
+                    dos.writeUTF(((TextSection) entry.getValue()).getContent());
+                }
+                if (entry.getValue() instanceof TextListSection) {
+                    TextListSection textListSection = (TextListSection) entry.getValue();
+                    dos.writeInt(textListSection.getLines().size());
+                    for (String s : textListSection.getLines()) {
+                        dos.writeUTF(s);
+                    }
+                }
+                if (entry.getValue() instanceof OrganizationSection) {
+                    OrganizationSection organizationSection = (OrganizationSection) entry.getValue();
+                    dos.writeInt(organizationSection.getOrganizations().size());
+                    for (Organization organization : organizationSection.getOrganizations()) {
+                        dos.writeUTF(organization.getName());
+                        dos.writeUTF(organization.getUrl());
+                        dos.writeInt(organization.getPositions().size());
+                        for (Position position : organization.getPositions()) {
+                            dos.writeUTF(position.getDateStart().toString());
+                            dos.writeUTF(position.getDateEnd().toString());
+                            dos.writeUTF(position.getTitle());
+                            dos.writeUTF(position.getText());
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -33,8 +64,71 @@ public class DataStreamSerializer implements StreamSerializer {
             for (int i = 0; i < size; i++) {
                 resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
             }
-            // TODO implements sections
+            size = dis.readInt();
+            for (int i = 0; i < size; i++) {
+                SectionType sectionType = SectionType.valueOf(dis.readUTF());
+                switch (sectionType) {
+                    case OBJECTIVE:
+                        addTextSection(dis, resume, sectionType);
+                        break;
+                    case PERSONAL:
+                        addTextSection(dis, resume, sectionType);
+                        break;
+                    case ACHIEVEMENT:
+                        addTextListSection(dis, resume, sectionType);
+                        break;
+                    case QUALIFICATIONS:
+                        addTextListSection(dis, resume, sectionType);
+                        break;
+                    case EDUCATION:
+                        addOrganizationSection(dis, resume, sectionType);
+                        break;
+                    case EXPERIENCE:
+                        addOrganizationSection(dis, resume, sectionType);
+                        break;
+                }
+            }
             return resume;
         }
+    }
+
+    private void addOrganizationSection(DataInputStream dis, Resume resume, SectionType sectionType) throws IOException {
+        int size = dis.readInt();
+        OrganizationSection organizationSection = new OrganizationSection();
+        List<Organization> organizations = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            Organization organization = new Organization();
+            organization.setName(dis.readUTF());
+            organization.setUrl(dis.readUTF());
+            int positionsSize = dis.readInt();
+            List<Position> positions = new ArrayList<>();
+            for (int n = 0; n < positionsSize; n++) {
+                Position position = new Position();
+                position.setDateStart(LocalDate.parse(dis.readUTF()));
+                position.setDateEnd(LocalDate.parse(dis.readUTF()));
+                position.setTitle(dis.readUTF());
+                position.setText(dis.readUTF());
+                positions.add(position);
+            }
+            organization.setPositions(positions);
+            organizations.add(organization);
+        }
+        organizationSection.setOrganizations(organizations);
+        resume.addSection(sectionType, organizationSection);
+    }
+
+    private void addTextListSection(DataInputStream dis, Resume resume, SectionType sectionType) throws IOException {
+        int size = dis.readInt();
+        TextListSection textListSection = new TextListSection();
+        List<String> lines = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            lines.add(dis.readUTF());
+        }
+        textListSection.setLines(lines);
+        resume.addSection(sectionType, textListSection);
+    }
+
+    private void addTextSection(DataInputStream dis, Resume resume, SectionType sectionType) throws IOException {
+        resume.addSection(sectionType, new TextSection(dis.readUTF()));
     }
 }
